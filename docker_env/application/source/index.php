83014e3b570246b48981282728c2f5dc1d98465e
Aurelien Saelens <aureliensaelens@MacBook-Air-de-Aurelien.local>
2021-11-25 12:40:33 +0100

    update

--- docker_env/application/source/index.php
+++ docker_env/application/source/index.php
@@ -1,58 +1,13 @@
 <?php
 include('server_connection.php');
-
+include('header.php');
 $sql = "SELECT * FROM `tutolink`";
 		$requete = $db->query($sql);
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<meta charset="UTF-8">
-	<meta name="description">
-	<script src="https://kit.fontawesome.com/8e9298d105.js" crossorigin="anonymous"></script>
-	
-	<link rel="stylesheet" href="style.css">
-	<title>Codeflix - Accueil</title>
-<body style="background-color: black;">
-	<header>
-		<div class="navbar">
-				<a class="navbar-brand" style="color: red;" href="#">CODEFLIX</a>
-				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-					<span class="navbar-toggler-icon"></span>
-				</button>
-				<div class="collapse navbar-collapse" id="navbarNavDropdown">
-					<ul class="navbar-nav">
-						<li class="nav-item active">
-							<a class="nav-link" href="#" style="color: white;">Tutos <span class="sr-only">(current)</span></a>
-						</li>
-						<li class="nav-item">
-							<a class="nav-link" style="color: white;" href="#">Documentation</a>
-						</li>
-						<li class="nav-item dropdown">
-							<a class="nav-link dropdown-toggle" style="color: white;" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-								Langage
-							</a>
-							<div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-								<a class="dropdown-item" href="#">HTML</a>
-								<a class="dropdown-item" href="#">CSS</a>
-								<a class="dropdown-item" href="#">JavaScript</a>
-								<a class="dropdown-item" href="#">PHP</a>
-							</div>
-						</li>
-					</ul>
-				</div>
-					<form class="form-inline my-2 my-lg-0">
-						<input class="form-control mr-sm-2" type="search" placeholder="Search"" aria-label="Search">
-						<button class="btn my-2 my-sm-0" type="submit" style="color: red;">Search</button>
-						<button class="btn ml-2" type="submit" style="color: red;">Profil</button>
-				</form>
-				</div>
-</div>
-		</header>
-	
-	<h1 style="color: red;">NodeJs</h1>
+<body>
+<?php include('navbar.php'); ?>
+<h1 style="color: red;">NodeJs</h1>
 	<div class="container" id="slider1">
 		<div class="slider">
 		<?php while($row = $requete->fetch(PDO::FETCH_ASSOC)) : ?>
@@ -66,12 +21,13 @@ $sql = "SELECT * FROM `tutolink`";
 				<a href="javascript::" class="arrow arrow-right">›</a>
 		</div>
 	</div>
-
-	<!-- FOOTER -->
-
-	<?php include('footer.php'); ?>
 </body>
 <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js'></script>
+<script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js'></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 
 <?php require_once "script.php";?>
+	
+</body>
 </html>
---
+++ docker_env/application/source/navbar.php
@@ -0,0 +1,37 @@
+<header>
+    <nav class="navbar navbar-expand-lg navbar-light bg-dark">
+        <a class="navbar-brand" style="color: red;" href="#">CODEFLIX</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+          <ul class="navbar-nav">
+            <li class="nav-item active">
+              <a class="nav-link" href="#" style="color: white;">Tutos <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" style="color: white;" href="#">Documentation</a>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" style="color: white;" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Langage
+              </a>
+              <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                <a class="dropdown-item" href="#">HTML</a>
+                <a class="dropdown-item" href="#">CSS</a>
+                <a class="dropdown-item" href="#">JavaScript</a>
+                <a class="dropdown-item" href="#">PHP</a>
+              </div>
+            </li>
+          </ul>
+        </div>
+          <form class="form-inline my-2 my-lg-0">
+            <input class="form-control mr-sm-2" type="search" method="post" placeholder="Search"" aria-label="Search">
+            <button class="btn my-2 my-sm-0" type="submit" style="color: red;">Search</button>
+            <button class="btn ml-2" type="submit" style="color: red;">Profil</button>
+			<button class="btn ml-2" type="submit" style="color: red;">Login</button>
+			<button class="btn ml-2" type="submit" style="color: red;">Logout</button>
+        </form>
+        </div>
+    </nav>
+</header>
