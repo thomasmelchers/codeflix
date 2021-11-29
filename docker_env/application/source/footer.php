12db43fdd36b2496d7d5231db3e1fbc4ebaf3c64
Thomas Melchers <thomas.melchers@gmail.com>
2021-11-29 08:56:37 +0100

    clarification des éléments html

--- docker_env/application/source/footer.php
+++ docker_env/application/source/footer.php
@@ -1,6 +1,7 @@
 <link rel="stylesheet" href="./css/footer.css">
 
-<footer class="container-fluid bg-dark">
+<footer>
+    <div class="container-fluid bg-dark">
         <div class="row p-0 mt-3 justify-content-around">
             <div class="col-2 text-center mt-md-2 icons-social">
                 <div>
@@ -54,10 +55,17 @@
             </div>
         </div>
         <div class="row p-0 mt-3 justify-content-center">
-                <div class="col-12  text-center">
-                    <p>Codeflix &copy; Aurélien, Marine, Nicolas & Thomas. </p>
-                </div>
+            <div class="col-12  text-center">
+                <p>Codeflix &copy; Aurélien, Marine, Nicolas & Thomas. </p>
             </div>
+        </div>
+    </div>
 </footer>
+
+<script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js'></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
+<?php require_once "script.php" ; ?>
 </body>
 </html>
--- docker_env/application/source/index.php
+++ docker_env/application/source/index.php
@@ -31,15 +31,9 @@ include('navbar.php');
 				<a href="javascript::" class="arrow arrow-right">›</a>
 		</div>
 	</div>
-</body>
-<script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js'></script>
-<script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js'></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-
+	
 <?php 
 // Inclure le footer
 include "footer.php";
-
-require_once "script.php";?>
+?>
 	
