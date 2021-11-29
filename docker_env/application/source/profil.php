2e06228b7cfcd297e959dfa4d05ecc14e7c3c810
MarineLLV <marine.lucottelevisage@gmail.com>
2021-11-29 19:59:36 +0100

    profile page

--- docker_env/application/source/profil.php
+++ docker_env/application/source/profil.php
@@ -11,6 +11,32 @@ include "navbar.php";
 
 <h1>Profil de <?= $_SESSION["utilisateur"]["pseudo"] ?></h1>
 
+<form method="post">
+    <div>
+        <label for="prenom">Prénom</label>
+        <input type="text" name="prenom" id="prenom">
+    </div>
+    <div>
+        <label for="nom">Nom</label>
+        <input type="text" name="nom" id="nom">
+    </div>
+    <div>
+        <label for="pseudo">Pseudo</label>
+        <input type="text" name="pseudo" id="pseudo">
+    </div>
+    <div>
+        <label for="email">Email</label>
+        <input type="email" name="email" id="email">
+    </div>
+    <div>
+        <label for="password">Mot de passe</label>
+        <input type="password" name="password" id="password">
+    </div>
+    <button type="submit">Je m'inscris !</button>
+</form>
+
+
+
 <p>Pseudo : <?= $_SESSION["utilisateur"]["pseudo"] ?> </p>
 <p>Email : <?= $_SESSION["utilisateur"]["email"] ?></p>
 
