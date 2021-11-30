08c18814cec6a141e32f7af6f29b913f32d70bf8
Aurelien Saelens <aureliensaelens@MacBook-Air-de-Aurelien.local>
2021-11-30 14:39:43 +0100

    update

--- docker_env/application/source/navbar.php
+++ docker_env/application/source/navbar.php
@@ -10,9 +10,6 @@
       <li class="nav-item active">
         <a class="nav-link" style="color: white;" href="#">Tutos <span class="sr-only">(current)</span></a>
       </li>
-      <li class="nav-item">
-        <a class="nav-link" style="color: white;" href="#">Documentation</a>
-      </li>
       <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="color: white;">
           Langage
