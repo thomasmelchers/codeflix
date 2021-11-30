5fa21517614e11e5e308057071ae500c3842df5f
MarineLLV <marine.lucottelevisage@gmail.com>
2021-11-30 23:30:18 +0100

    modif' session + script navbar

--- docker_env/application/source/profil.php
+++ docker_env/application/source/profil.php
@@ -9,37 +9,36 @@ include "header.php";
 include "navbar.php";
 ?>
 
-<h1>Profil de <?= $_SESSION["utilisateur"]["pseudo"] ?></h1>
+<link rel="stylesheet" href="./css/addUser.css">
+<!-- <link rel="stylesheet" href="./css/add_user.css"> -->
 
-<form method="post">
-    <div>
-        <label for="prenom">Prénom</label>
-        <input type="text" name="prenom" id="prenom">
-    </div>
-    <div>
-        <label for="nom">Nom</label>
-        <input type="text" name="nom" id="nom">
-    </div>
-    <div>
-        <label for="pseudo">Pseudo</label>
-        <input type="text" name="pseudo" id="pseudo">
-    </div>
-    <div>
-        <label for="email">Email</label>
-        <input type="email" name="email" id="email">
-    </div>
-    <div>
-        <label for="password">Mot de passe</label>
-        <input type="password" name="password" id="password">
-    </div>
-    <button type="submit">Je m'inscris !</button>
-</form>
-
-
-
-<p>Pseudo : <?= $_SESSION["utilisateur"]["pseudo"] ?> </p>
-<p>Email : <?= $_SESSION["utilisateur"]["email"] ?></p>
-
+<!-- Form html -->
+        <h1>Profil de <?= $_SESSION["utilisateur"]["pseudo"] ?></h1>
+        <div class="formulaire">
+            <form method="post">
+                <div>
+                    <label for="prenom">Prénom</label>
+                    <input type="text" name="prenom" id="prenom" placeholder="<?= $_SESSION["utilisateur"]["prenom"] ?>" >
+                </div>
+                <div>
+                    <label for="nom">Nom</label>
+                    <input type="text" name="nom" id="nom" placeholder="<?= $_SESSION["utilisateur"]["nom"] ?>">
+                </div>
+                <div>
+                    <label for="pseudo">Pseudo</label>
+                    <input type="text" name="pseudo" id="pseudo" placeholder="<?= $_SESSION["utilisateur"]["pseudo"] ?>">
+                </div>
+                <div>
+                    <label for="email">Email</label>
+                    <input type="email" name="email" id="email" placeholder="<?= $_SESSION["utilisateur"]["email"] ?>">
+                </div>
+                <div>
+                    <label for="password">Mot de passe</label>
+                    <input type="password" name="password" id="password" placeholder="<?= $_SESSION["utilisateur"]["password"] ?>">
+                </div>
+                <button class="btn" type="submit" name="submit" id="submit">Mettre à jour</button>
+            </form>
+        </div>
 <?php
 // Inclure le footer
 include "footer.php";
