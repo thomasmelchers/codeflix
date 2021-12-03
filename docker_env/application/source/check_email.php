6be7997e1293ddf83410b2564c1502f857e27114
birennicolas <90913609+birennicolas@users.noreply.github.com>
2021-12-03 16:14:32 +0100

    modif passwordlost

--- docker_env/application/source/check_email.php
+++ docker_env/application/source/check_email.php
@@ -68,7 +68,7 @@ if(isset($_POST['reset_link'])){
     
 
 <!-- Log in Form & Reset -->
-    <div class="logocheck" style="text-align: center; padding-top: 200px">
+    <div class="logocheck" style="text-align: center; padding-top: 10%">
         <img src="logos/logo_small.png">
     </div>
         <div class="container">
